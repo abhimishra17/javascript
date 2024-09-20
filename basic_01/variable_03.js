3bc3bb10a32eafe83ae465250aadc10bb7ec6a3f
Abhishek Kumar Mishra <153258500+abhimishra17@users.noreply.github.com>
2024-09-20 13:33:09 +0000

    var conversion

---
+++ basic_01/variable_03.js
@@ -0,0 +1,12 @@
+//conversion of datatype:
+let score=33;
+console.log(typeof(score));
+console.log(typeof score);
+score="33"
+console.log(typeof(score));
+let Value=Number(score);
+console.log(typeof(Value));
+let x="3"+1;
+console.log(x);
+let y="3"+"5";
+console.log(y);
