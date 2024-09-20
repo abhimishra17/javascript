56173da35f3ae626c5996603ad7d0c3d69042a66
Abhishek Kumar Mishra <153258500+abhimishra17@users.noreply.github.com>
2024-09-20 12:46:57 +0000

    variable

---
+++ basic_01/variables_02.js
@@ -0,0 +1,10 @@
+const accountId = 1444523; 
+let accountEmail="abhishek@gmail.com";
+var accountpassword="12345";
+accountcity="jaipur";
+//accountId=2;
+ccountEmail="abhik@gmail.com";
+ accountpassword="123459393";
+accountcity="delhi"
+console.log(accountId);
+console.table([accountId,accountEmail,accountpassword,accountcity]);
